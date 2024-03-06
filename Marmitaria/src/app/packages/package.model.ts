850dcc1e1fbc52098a2f9f83ef2890caeeb56df0
ilailsonrocha <ilailsonrocha@iec.gov.br>
2024-03-06 17:24:06 -0300

    Vinculado ingrentes...Marmitas

--- Marmitaria/src/app/packages/package.model.ts
+++ Marmitaria/src/app/packages/package.model.ts
@@ -1,12 +1,15 @@
+import { Food } from "../shared/food.model";
+
 export class Package {
     public name: string;
     public description: string;
     public imagePath: string;
+    public ingredientes: Food[];
     
-    
-    constructor(name: string, description: string, imagePath:string){
+    constructor(name: string, description: string, imagePath:string, ingredientes: Food[]){
         this.name = name;
         this.description=description;
         this.imagePath = imagePath;   
+        this.ingredientes = ingredientes;
     }
 }
--- Marmitaria/src/app/packages/package.service.ts
+++ Marmitaria/src/app/packages/package.service.ts
@@ -1,13 +1,30 @@
 import { EventEmitter } from "@angular/core";
 import { Package } from "./package.model";
+import { Food } from "../shared/food.model";
 
 export class PackageService{
 
     packageSelected = new EventEmitter<Package>();
 
     private packages: Package [] = [
-        new Package('Churrasco', 'Churraco de fraudinha','https://as2.ftcdn.net/v2/jpg/04/26/44/83/1000_F_426448379_eJRZjHMbhKJKxtpbj76rV1wTIljfAHCe.jpg'),
-        new Package('Salmão', 'Uma delicia','https://img.freepik.com/fotos-gratis/salmao-grelhado-com-salada_2829-19756.jpg?t=st=1709152629~exp=1709153229~hmac=bc4af3cc9590a40bdd2b281bd26df70c9c69dd7ba9ff8377b9f1dae33b50f2a3')
+        new Package(
+            'Churrasco',
+            'Churraco de fraudinha',
+            'https://as2.ftcdn.net/v2/jpg/04/26/44/83/1000_F_426448379_eJRZjHMbhKJKxtpbj76rV1wTIljfAHCe.jpg',
+            [
+                new Food('Picanha',1),
+                new Food('Sal',1)
+            ]
+            ),
+        new Package(
+            'Salmão',
+            'Uma delicia',
+            'https://img.freepik.com/fotos-gratis/salmao-grelhado-com-salada_2829-19756.jpg?t=st=1709152629~exp=1709153229~hmac=bc4af3cc9590a40bdd2b281bd26df70c9c69dd7ba9ff8377b9f1dae33b50f2a3',
+            [
+                new Food('Salmão',1),
+                new Food('Sal',1)
+            ]
+            )
     ];
 
     getPackages(){
